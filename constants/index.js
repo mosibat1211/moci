93adbc52f0d116664cd23a80b28acc6382cb10de
mosibat1211 <92864493+mosibat1211@users.noreply.github.com>
2023-07-19 19:29:00 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xa97c678b017d4b01df30fe4c85c25856ae636826";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
